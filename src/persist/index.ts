bd559b706241dcd60ef1ef8223160695cb6458d1
Mohammad Kaleem <mohammadkaleem5556@gmail.com>
2024-07-23 02:05:37 +0530

    persist file

---
+++ src/persist/index.ts
@@ -0,0 +1,39 @@
+export const persistReducer = (rootReducer) => (state, action) => {
+    if (action.type === "LOAD") {
+        return { ...state, ...action.payload };
+    }
+    return rootReducer(state, action);
+};
+
+export const persistInitialize = (store) => {
+    window.addEventListener("beforeunload", () => {
+        console.log("beforeunload",);
+        localStorage.setItem("my-store", JSON.stringify(store.getState()));
+    });
+
+    window.addEventListener("load", () => {
+        console.log("load");
+        const myState = JSON.parse(localStorage.getItem("my-store"));
+        if (myState) {
+            store.dispatch({ type: "LOAD", payload: myState });
+            localStorage.removeItem("my-store");
+        }
+    });
+
+    window.addEventListener("visibilitychange", () => {
+        // console.log("visibilitychange", document.visibilityState, event);
+
+        if (document.visibilityState === "hidden") {
+            localStorage.setItem("my-store", JSON.stringify(store.getState()));
+        }
+
+        if (document.visibilityState === "visible") {
+            const data = localStorage.getItem("my-store");
+            if (data) {
+                let state = JSON.parse(data);
+                store.dispatch({ type: "LOAD", payload: state });
+                localStorage.removeItem("my-store");
+            }
+        }
+    });
+};
