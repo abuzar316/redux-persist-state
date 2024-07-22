4a7315dee4254bcf2f1b56a09b47d88c89bb835d
Mohammad Kaleem <mohammadkaleem5556@gmail.com>
2024-07-23 02:09:36 +0530

    changes

--- src/persist/index.ts
+++ src/persist/index.ts
@@ -1,39 +1,41 @@
-export const persistReducer = (rootReducer) => (state, action) => {
-    if (action.type === "LOAD") {
-        return { ...state, ...action.payload };
+const persistReducer = (rootReducer) => (state, action) => {
+  if (action.type === "LOAD") {
+    return { ...state, ...action.payload };
+  }
+  return rootReducer(state, action);
+};
+
+const persistInitialize = (store) => {
+  window.addEventListener("beforeunload", () => {
+    console.log("beforeunload");
+    localStorage.setItem("my-store", JSON.stringify(store.getState()));
+  });
+
+  window.addEventListener("load", () => {
+    console.log("load");
+    const myState = JSON.parse(localStorage.getItem("my-store"));
+    if (myState) {
+      store.dispatch({ type: "LOAD", payload: myState });
+      localStorage.removeItem("my-store");
     }
-    return rootReducer(state, action);
+  });
+
+  window.addEventListener("visibilitychange", () => {
+    // console.log("visibilitychange", document.visibilityState, event);
+
+    if (document.visibilityState === "hidden") {
+      localStorage.setItem("my-store", JSON.stringify(store.getState()));
+    }
+
+    if (document.visibilityState === "visible") {
+      const data = localStorage.getItem("my-store");
+      if (data) {
+        let state = JSON.parse(data);
+        store.dispatch({ type: "LOAD", payload: state });
+        localStorage.removeItem("my-store");
+      }
+    }
+  });
 };
 
-export const persistInitialize = (store) => {
-    window.addEventListener("beforeunload", () => {
-        console.log("beforeunload",);
-        localStorage.setItem("my-store", JSON.stringify(store.getState()));
-    });
-
-    window.addEventListener("load", () => {
-        console.log("load");
-        const myState = JSON.parse(localStorage.getItem("my-store"));
-        if (myState) {
-            store.dispatch({ type: "LOAD", payload: myState });
-            localStorage.removeItem("my-store");
-        }
-    });
-
-    window.addEventListener("visibilitychange", () => {
-        // console.log("visibilitychange", document.visibilityState, event);
-
-        if (document.visibilityState === "hidden") {
-            localStorage.setItem("my-store", JSON.stringify(store.getState()));
-        }
-
-        if (document.visibilityState === "visible") {
-            const data = localStorage.getItem("my-store");
-            if (data) {
-                let state = JSON.parse(data);
-                store.dispatch({ type: "LOAD", payload: state });
-                localStorage.removeItem("my-store");
-            }
-        }
-    });
-};
+export { persistReducer, persistInitialize };
